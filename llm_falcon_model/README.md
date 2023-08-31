4846dcec35c3d135efc3ee4501d9c2c941651060
Hristo Vrigazov <hvrigazov@gmail.com>
2023-08-31 16:30:30 +0300

    Initialization of new projects.

---
+++ llm_falcon_model/README.md
@@ -0,0 +1,16 @@
+# LLM Sampler
+
+Install with:
+
+```bash
+pip install llm_falcon_model
+```
+
+[![Downloads](https://static.pepy.tech/badge/llm_falcon_model/month)](https://pepy.tech/project/llm_falcon_model)
+[![PyPi version](https://badgen.net/pypi/v/llm_falcon_model/)](https://pypi.com/project/llm_falcon_model)
+[![PyPI license](https://img.shields.io/pypi/l/llm_falcon_model.svg)](https://pypi.python.org/pypi/llm_falcon_model/)
+
+
+## Contents
+
+TODO
---
+++ llm_weights_mmap/src/weights_separation.py
@@ -0,0 +1,31 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import torch
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Dump each key in a .pth file to a separate .npy file')
+    parser.add_argument('--pth_files_dir', metavar='PTH_FILES_DIR', type=str, help='Path to the .pth files directory', required=True)
+    parser.add_argument('--output_dir', metavar='OUTPUT_DIR', type=str, help='Output directory to dump the keys', required=True)
+    args = parser.parse_args()
+
+    output_dir = Path(args.output_dir)
+    pth_files_dir = Path(args.pth_files_dir)
+    output_dir.mkdir(exist_ok=True)
+    pth_files_dir.mkdir(exist_ok=True)
+
+    model_name = pth_files_dir.name
+    (output_dir / model_name).mkdir(exist_ok=True)
+    for pth_file in sorted(pth_files_dir.glob('*.pth')):
+        print(f'Processing pth file "{pth_file.resolve()}"')
+        (output_dir / model_name / pth_file.stem).mkdir(exist_ok=True, parents=True)
+        for k, v in torch.load(pth_file, map_location="cpu").items():
+            print(f'\tProcessing key "{k}"')
+            filename_path = output_dir / model_name / pth_file.stem / f'{k}.npy'
+            np.save(filename_path, v.numpy())
+
+
+if __name__ == '__main__':
+    main()
