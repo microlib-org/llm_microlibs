34198ffafccd86cac88e620451892ab3d07d7aa9
Hristo Vrigazov <hvrigazov@gmail.com>
2023-09-11 09:57:40 +0300

    Added comment.

--- llm_falcon_model/src/llm_falcon_model/tokenization.py
+++ llm_falcon_model/src/llm_falcon_model/tokenization.py
@@ -4,5 +4,6 @@ from tokenizers import Tokenizer
 
 
 def load_tokenizer() -> Tokenizer:
+    # Both 7b and 40b use the same tokenizer
     json_file = Path(__file__).parent / f'falcon7b_tokenizer.json'
     return Tokenizer.from_file(str(json_file))
