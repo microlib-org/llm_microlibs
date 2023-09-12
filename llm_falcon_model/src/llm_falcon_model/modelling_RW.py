fd8793de30afbfae99a0c796ab379efe16e4c96d
Hristo Vrigazov <hvrigazov@gmail.com>
2023-09-12 09:29:36 +0300

    Falcon 40B modeling

--- llm_falcon_model/src/llm_falcon_model/modelling_RW.py
+++ llm_falcon_model/src/llm_falcon_model/modelling_RW.py
@@ -5,9 +5,11 @@ from typing import Optional, Tuple
 
 import torch
 import torch.utils.checkpoint
+from einops import rearrange
 from torch import nn
 from torch.nn import LayerNorm
 from torch.nn import functional as F
+
 
 class Linear(nn.Linear):
     def forward(self, input: torch.Tensor) -> torch.Tensor:
@@ -20,7 +22,7 @@ class Linear(nn.Linear):
 
 # rotary pos emb helpers (torch.jit.script does not seem to support staticmethod...)
 def rotate_half(x):
-    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
+    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2:]
     return torch.cat((-x2, x1), dim=x1.ndim - 1)  # dim=-1 triggers a bug in torch < 1.8.0
 
 
@@ -31,9 +33,9 @@ class RotaryEmbedding(torch.nn.Module):
     """
 
     def __init__(
-        self,
-        head_dim: int,
-        base=10000,
+            self,
+            head_dim: int,
+            base=10000,
     ):
         super().__init__()
         inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
@@ -45,10 +47,10 @@ class RotaryEmbedding(torch.nn.Module):
         self.sin_cached: torch.Tensor | None = None
 
     def cos_sin(
-        self,
-        seq_len: int,
-        device="cuda",
-        dtype=torch.bfloat16,
+            self,
+            seq_len: int,
+            device="cuda",
+            dtype=torch.bfloat16,
     ) -> torch.Tensor:
         if seq_len != self.seq_len_cached:
             self.seq_len_cached = seq_len
@@ -79,7 +81,7 @@ def dropout_add(x: torch.Tensor, residual: torch.Tensor, prob: float, training: 
     return out
 
 
-class Attention(nn.Module):
+class Attention7B(nn.Module):
     def __init__(self, config):
         super().__init__()
 
@@ -133,8 +135,8 @@ class Attention(nn.Module):
             return fused_qkv[..., :-2, :], fused_qkv[..., [-2], :], fused_qkv[..., [-1], :]
 
     def forward(
-        self,
-        hidden_states: torch.Tensor,
+            self,
+            hidden_states: torch.Tensor,
     ):
         fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
 
@@ -172,6 +174,137 @@ class Attention(nn.Module):
         return outputs
 
 
+class Attention40B(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.hidden_size = config.hidden_size
+        self.num_heads = config.n_head
+        self.head_dim = self.hidden_size // self.num_heads
+        self.split_size = self.hidden_size
+        self.hidden_dropout = config.hidden_dropout
+
+        if self.head_dim * self.num_heads != self.hidden_size:
+            raise ValueError(
+                f"`hidden_size` must be divisible by num_heads (got `hidden_size`: {self.hidden_size} and `num_heads`:"
+                f" {self.num_heads})."
+            )
+
+        self.maybe_rotary = RotaryEmbedding(config.head_dim) if config.rotary else lambda q, k: (q, k)
+
+        # Layer-wise attention scaling
+        self.inv_norm_factor = 1.0 / math.sqrt(self.head_dim)
+        self.beta = self.inv_norm_factor
+
+        self.query_key_value = Linear(
+            self.hidden_size,
+            (config.n_head_kv * 2 + config.n_head) * self.head_dim,
+            bias=config.bias,
+        )
+        self.dense = Linear(self.hidden_size, self.hidden_size, bias=config.bias)
+        self.attention_dropout = nn.Dropout(config.attention_dropout)
+        self.num_kv = config.n_head_kv
+
+    def _split_heads(self, fused_qkv: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Split the last dimension into (num_heads, head_dim), results share same memory
+        storage as `fused_qkv`
+
+        Args:
+            fused_qkv (`torch.tensor`, *required*): [batch_size, seq_length, num_heads * 3 * head_dim]
+
+        Returns:
+            query: [batch_size, seq_length, num_heads, head_dim]
+            key: [batch_size, seq_length, num_heads, head_dim]
+            value: [batch_size, seq_length, num_heads, head_dim]
+        """
+        batch, seq_len, _ = fused_qkv.shape
+        qkv = fused_qkv.view(batch, seq_len, -1, self.num_heads // self.num_kv + 2, 64)
+        q = qkv[:, :, :, :-2]
+        k = qkv[:, :, :, [-2]]
+        v = qkv[:, :, :, [-1]]
+        k = torch.broadcast_to(k, q.shape)
+        v = torch.broadcast_to(v, q.shape)
+
+        q, k, v = [
+            rearrange(
+                x,
+                "batch seq_len group num_heads head_dim ->\
+                batch seq_len (group num_heads) head_dim",
+                head_dim=self.head_dim,
+            )
+            for x in [q, k, v]
+        ]
+        return q, k, v
+
+    def _merge_heads(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Merge heads together over the last dimenstion
+
+        Args:
+            x: (`torch.tensor`, *required*): [batch_size * num_heads, seq_length, head_dim]
+
+        Returns:
+            torch.tensor: [batch_size, seq_length, num_heads * head_dim]
+        """
+        # What we want to achieve is:
+        # batch_size * num_heads, seq_length, head_dim -> batch_size, seq_length, num_heads * head_dim
+        batch_size_and_num_heads, seq_length, _ = x.shape
+        batch_size = batch_size_and_num_heads // self.num_heads
+
+        # First view to decompose the batch size
+        # batch_size * num_heads, seq_length, head_dim -> batch_size, num_heads, seq_length, head_dim
+        x = x.view(batch_size, self.num_heads, seq_length, self.head_dim)
+
+        # batch_size, num_heads, seq_length, head_dim -> batch_size, seq_length, num_heads, head_dim
+        x = x.permute(0, 2, 1, 3)
+
+        # batch_size, seq_length, num_heads, head_dim -> batch_size, seq_length, num_heads * head_dim
+        return x.reshape(batch_size, seq_length, self.num_heads * self.head_dim)
+
+    def forward(
+            self,
+            hidden_states: torch.Tensor,
+    ):
+        fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
+
+        # 3 x [batch_size, seq_length, num_heads, head_dim]
+        (query_layer, key_layer, value_layer) = self._split_heads(fused_qkv)
+
+        batch_size, q_length, _, _ = query_layer.shape
+
+        query_layer = query_layer.transpose(1, 2).reshape(batch_size * self.num_heads, q_length, self.head_dim)
+        key_layer = key_layer.transpose(1, 2).reshape(
+            batch_size * self.num_heads,
+            q_length,
+            self.head_dim,
+        )
+        value_layer = value_layer.transpose(1, 2).reshape(batch_size * self.num_heads, q_length, self.head_dim)
+
+        query_layer, key_layer = self.maybe_rotary(query_layer, key_layer)
+
+        _, kv_length, _ = key_layer.shape
+
+        present = None
+
+        query_layer_ = query_layer.reshape(batch_size, self.num_heads, -1, self.head_dim)
+        key_layer_ = key_layer.reshape(batch_size, self.num_heads, -1, self.head_dim)
+        value_layer_ = value_layer.reshape(batch_size, self.num_heads, -1, self.head_dim)
+
+        attn_output = F.scaled_dot_product_attention(
+            query_layer_, key_layer_, value_layer_, None, 0.0, is_causal=True
+        )
+
+        x = attn_output.view(batch_size, self.num_heads, q_length, self.head_dim)
+        x = x.permute(0, 2, 1, 3)
+        attn_output = x.reshape(batch_size, q_length, self.num_heads * self.head_dim)
+
+        output_tensor = self.dense(attn_output)
+
+        outputs = (output_tensor, present)
+        return outputs
+
+
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -188,14 +321,14 @@ class MLP(nn.Module):
         return x
 
 
-class DecoderLayer(nn.Module):
+class DecoderLayer7B(nn.Module):
     def __init__(self, config):
         super().__init__()
         hidden_size = config.hidden_size
 
         self.input_layernorm = LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
         self.num_heads = config.n_head
-        self.self_attention = Attention(config)
+        self.self_attention = Attention7B(config)
 
         if not config.parallel_attn:
             # unused if parallel attn
@@ -209,8 +342,8 @@ class DecoderLayer(nn.Module):
         self.config = config
 
     def forward(
-        self,
-        hidden_states: torch.Tensor,
+            self,
+            hidden_states: torch.Tensor,
     ):
 
         layernorm_output = self.input_layernorm(hidden_states)
@@ -234,6 +367,60 @@ class DecoderLayer(nn.Module):
         return outputs  # hidden_states, present, attentions
 
 
+class DecoderLayer40B(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        hidden_size = config.hidden_size
+
+        self.ln_attn = LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
+        self.ln_mlp = LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
+
+        self.num_heads = config.n_head
+        self.self_attention = Attention40B(config)
+
+        self.mlp = MLP(config)
+
+        self.apply_residual_connection_post_layernorm = config.apply_residual_connection_post_layernorm
+        self.hidden_dropout = config.hidden_dropout
+
+        self.config = config
+
+    def forward(
+            self,
+            hidden_states: torch.Tensor,
+    ):
+
+        ln_attn = self.ln_attn(hidden_states)
+        ln_mlp = self.ln_mlp(hidden_states)
+
+        residual = hidden_states
+
+        # Self attention.
+        attn_outputs = self.self_attention(ln_attn)
+
+        attention_output = attn_outputs[0]
+
+        outputs = attn_outputs[1:]
+
+        # MLP.
+        mlp_output = self.mlp(ln_mlp)
+
+        output = dropout_add(
+            mlp_output + attention_output, residual, self.config.hidden_dropout, training=self.training
+        )
+
+        outputs = (output,) + outputs[1:]
+        return outputs  # hidden_states, present, attentions
+
+
+def get_layer_class(model_generation):
+    if model_generation == '7b':
+        return DecoderLayer7B
+    if model_generation == '40b':
+        return DecoderLayer40B
+    raise ValueError(f"Unknown model generation: '{model_generation}'")
+
+
 class RWModel(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -246,7 +433,9 @@ class RWModel(nn.Module):
         self.word_embeddings = nn.Embedding(config.vocab_size, self.embed_dim)
 
         # Transformer blocks
-        self.h = nn.ModuleList([DecoderLayer(config) for _ in range(config.num_hidden_layers)])
+        model_generation = config._name_or_path.split('/')[1].split('-')[1]
+        layer_class = get_layer_class(model_generation)
+        self.h = nn.ModuleList([layer_class(config) for _ in range(config.num_hidden_layers)])
 
         # Final Layer Norm
         self.ln_f = LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
@@ -288,7 +477,9 @@ class FalconStart(nn.Module):
         self.word_embeddings = nn.Embedding(config.vocab_size, self.embed_dim)
 
         # Transformer blocks
-        self.h = nn.ModuleDict({str(i): DecoderLayer(config) for i in range(n_transformer_layers)})
+        model_generation = config._name_or_path.split('/')[1].split('-')[1]
+        layer_class = get_layer_class(model_generation)
+        self.h = nn.ModuleDict({str(i): layer_class(config) for i in range(n_transformer_layers)})
 
     def forward(self, input_ids: Optional[torch.LongTensor]) -> Tuple[torch.Tensor, ...]:
         inputs_embeds = self.word_embeddings(input_ids)
@@ -303,7 +494,9 @@ class FalconMid(nn.Module):
     def __init__(self, config, start_layer: int, end_layer: int):
         super().__init__()
         # Transformer blocks
-        self.h = nn.ModuleDict({str(i): DecoderLayer(config) for i in range(start_layer, end_layer)})
+        model_generation = config._name_or_path.split('/')[1].split('-')[1]
+        layer_class = get_layer_class(model_generation)
+        self.h = nn.ModuleDict({str(i): layer_class(config) for i in range(start_layer, end_layer)})
 
     def forward(self, hidden_states: Optional[torch.Tensor]) -> Tuple[torch.Tensor, ...]:
         for i, block in self.h.items():
@@ -318,8 +511,9 @@ class FalconEnd(nn.Module):
 
         self.embed_dim = config.hidden_size
         # Transformer blocks
-        n_transformer_blocks = config.num_hidden_layers - start_layer
-        self.h = nn.ModuleList({str(i): DecoderLayer(config) for i in range(n_transformer_blocks)})
+        model_generation = config._name_or_path.split('/')[1].split('-')[1]
+        layer_class = get_layer_class(model_generation)
+        self.h = nn.ModuleList({str(i): layer_class(config) for i in range(start_layer, config.num_hidden_layers)})
 
         # Final Layer Norm
         self.ln_f = LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
