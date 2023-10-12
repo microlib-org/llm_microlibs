3ee8a1c6932bed2b74de6c51e2082745b37ae2c4
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-12 16:51:44 +0300

    Rename.

--- llm_falcon_model/src/llm_falcon_model/__init__.py
+++ llm_falcon_model/src/llm_falcon_model/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.5.0"
 from .modelling_RW import FalconStart, FalconMid, FalconEnd, FalconFull
 from .configuration_RW import load_config
 from .tokenization import load_tokenizer
-from .initialization import init_mid
+from .initialization import init_part
 
