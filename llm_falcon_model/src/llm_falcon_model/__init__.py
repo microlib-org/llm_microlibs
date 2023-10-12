47dff8aa70eb061ef57031acd51470681ae9cf18
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-12 15:37:00 +0300

    Utility for initialization of part.

--- llm_falcon_model/src/llm_falcon_model/__init__.py
+++ llm_falcon_model/src/llm_falcon_model/__init__.py
@@ -3,3 +3,5 @@ __version__ = "0.3.0"
 from .modelling_RW import FalconStart, FalconMid, FalconEnd, FalconFull
 from .configuration_RW import load_config
 from .tokenization import load_tokenizer
+from .initialization import initialize_part
+
