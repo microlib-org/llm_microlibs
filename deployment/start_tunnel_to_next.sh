ae22326f658632feb589d6310afcfd36a5b116d2
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-12 14:56:33 +0300

    Initialize new package.

--- deployment/start_tunnel_to_next.sh
+++ deployment/start_tunnel_to_next.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
+
+# Check if argument is not supplied
+if [ -z "$1" ]; then
+    echo "Usage: $0 <hostname>"
+    echo "Please provide the hostname as an argument."
+    exit 1
+fi
+
 NEXT_HOST=$1
 ssh -L 61001:127.0.0.1:61000 "$NEXT_HOST"
