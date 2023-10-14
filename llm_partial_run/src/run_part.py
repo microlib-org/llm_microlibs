d5dbbb268ece19e4664a3c91835b20bf9dafa0ae
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-15 00:13:05 +0300

    Logging improvement.

--- llm_partial_run/src/run_part.py
+++ llm_partial_run/src/run_part.py
@@ -86,7 +86,9 @@ class PartialRun:
                 self.next_layer(computation_id, x.detach().cpu().half().numpy())
                 logging.info(f'{self.layer_prefix} are done processing.')
             else:
+                logging.info(f'{self.layer_prefix} saving result to "{self.out_path}" ...')
                 np.save(self.out_path / f'{computation_id}.npy', x.detach().cpu().half().numpy())
+                logging.info(f'{self.layer_prefix} is done saving.')
             if self.cpu_state_dicts is not None:
                 logging.info('Moving next state dict to GPU ...')
                 start_t = time.time()
