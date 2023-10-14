6194c312622c8734e24d23747ae4094b4200cb79
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-14 22:46:45 +0300

    Move to another module.

--- llm_partial_run/src/run_part.py
+++ llm_partial_run/src/run_part.py
@@ -11,27 +11,7 @@ import numpy as np
 import torch
 from socket_rpc import rpc, rpc_call
 
-
-def parse_ranges(input_string: str) -> List[Tuple]:
-    tokens = input_string.split()
-    try:
-        numbers = [int(token) for token in tokens]
-    except ValueError:
-        raise ValueError("Input string contains non-integer values. "
-                         "You need to provide a list of intervals, e.g. '0 5 10 15'")
-    if len(numbers) % 2 != 0:
-        raise ValueError("Input string contains an odd number of integers. "
-                         "You need to provide a list of intervals, e.g. '0 5 10 15'")
-    ranges = [(numbers[i], numbers[i + 1]) for i in range(0, len(numbers), 2)]
-    if len(ranges) <= 0:
-        raise ValueError("At least one interval is required.")
-    if ranges[0][0] == 0 and len(ranges) > 1:
-        raise ValueError("If the first interval starts with 0, only one interval is allowed.")
-    range_sizes = [b - a for a, b in ranges]
-    if len(set(range_sizes)) > 1:
-        raise ValueError("Intervals are of different sizes. "
-                         "You need to provide a list of intervals, e.g. '0 5 10 15'")
-    return ranges
+from llm_partial_run.ranges import parse_ranges
 
 
 def load_cpu_state_dicts(init_part, model_name, separated_weights_path, ranges) -> List[Dict]:
