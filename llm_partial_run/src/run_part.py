158e16a2b74b68c73d4e8fc0031fcf417f271b44
Hristo Vrigazov <hvrigazov@gmail.com>
2023-10-14 18:33:38 +0300

    Parse multiple ranges.

--- llm_partial_run/src/run_part.py
+++ llm_partial_run/src/run_part.py
@@ -5,11 +5,31 @@ import socket
 import time
 import traceback
 from pathlib import Path
-from typing import Callable, Optional
+from typing import Callable, Optional, Tuple, List
 
 import numpy as np
 import torch
 from socket_rpc import rpc, rpc_call
+
+
+def parse_ranges(input_string: str) -> List[Tuple]:
+    tokens = input_string.split()
+    try:
+        numbers = [int(token) for token in tokens]
+    except ValueError:
+        raise ValueError("Input string contains non-integer values. "
+                         "You need to provide a list of intervals, e.g. '0 5 10 15'")
+    if len(numbers) % 2 != 0:
+        raise ValueError("Input string contains an odd number of integers. "
+                         "You need to provide a list of intervals, e.g. '0 5 10 15'")
+    ranges = [(numbers[i], numbers[i + 1]) for i in range(0, len(numbers), 2)]
+    if ranges[0][0] == 0 and len(ranges) > 1:
+        raise ValueError("If the first interval starts with 0, only one interval is allowed.")
+    range_sizes = [b - a for a, b in ranges]
+    if len(set(range_sizes)) > 1:
+        raise ValueError("Intervals are of different sizes. "
+                         "You need to provide a list of intervals, e.g. '0 5 10 15'")
+    return ranges
 
 
 def run_partial(
@@ -25,8 +45,8 @@ def run_partial(
         out_path: Optional[Path],
         layers_zfill: int = 3
 ):
-    layers_list = tuple(map(int, layers.split(' ')))
-    start_layer, end_layer = layers_list
+    ranges: List[Tuple] = parse_ranges(layers)
+    start_layer, end_layer = ranges[0]
     module = initialization_func(model_name, start_layer, end_layer, separated_weights_path, device)
     layer_range_str = f'{str(start_layer).zfill(layers_zfill)}-{str(end_layer).zfill(layers_zfill)}'
     layer_prefix = f'Layers {layer_range_str} on {socket.gethostname()}'
