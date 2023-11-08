3a0e61afa1b41a1d19934059f2e7c8008533fffa
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-08 22:10:47 +0200

    Run test for 7b, 40b and 180b.

--- llm_falcon_model/tests/test_initializations.py
+++ llm_falcon_model/tests/test_initializations.py
@@ -1,11 +1,12 @@
+import pytest
 import torch
 
 from llm_falcon_model import FalconStart, load_config
 
 
-def test_initialize_start():
-    config = load_config('7b')
+@pytest.mark.parametrize("model_name", ["7b", "40b", "180b"])
+def test_initialize_start(model_name):
+    config = load_config(model_name)
     torch.set_default_device(torch.device("cuda:0"))
     torch.set_default_dtype(torch.bfloat16)
     start = FalconStart(config, 4).eval()
-
