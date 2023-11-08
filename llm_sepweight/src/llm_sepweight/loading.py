df1815ec6815735cabc5a31e7bcc19e492fff078
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-08 23:09:45 +0200

    Loading functions.

--- llm_sepweight/src/llm_sepweight/loading.py
+++ llm_sepweight/src/llm_sepweight/loading.py
@@ -1,3 +1,5 @@
+import logging
+
 from pathlib import Path
 from typing import Union, Tuple
 
@@ -8,31 +10,40 @@ import torch
 def load_np_dir_as_state_dict(prefix: str, path: Union[str, Path], dtype: torch.dtype):
     path = Path(path)
     assert path.is_dir(), f"The supplied directory {path} does not exist!"
+    logging.info(f'Prefix: "{prefix}", loading np directory: {path} ...')
     res = {}
     for child in path.glob('*.npy'):
-        res[f'{prefix}{child.stem}'] = torch.tensor(np.load(str(child)), dtype=dtype)
+        logging.info(f'Prefix: "{prefix}", loading np key: {child} ...')
+        res[f'{prefix}.{child.stem}'] = torch.tensor(np.load(str(child)), dtype=dtype)
     return res
 
 
 def load_mid_as_state_dict(
+        prefix: str,
         path: Path,
-        part: Tuple[str]
+        part: Tuple[str],
+        dtype: torch.dtype
 ):
     start = int(part[1])
     end = int(part[2])
+    logging.info(f'Prefix: "{prefix}", loading layers {start} to {end} ...')
+    state_dict = {}
     for i in range(start, end):
-        path / 'mid' / str(i)
+        layer_prefix = f'{prefix}.h.{i}'
+        layer_state_dict = load_np_dir_as_state_dict(layer_prefix, path / 'mid' / str(i), dtype)
+        state_dict.update(layer_state_dict)
+    return state_dict
 
 
 def load_as_state_dict(
         path: Union[str, Path],
-        part: Tuple[str]
+        part: Tuple[str],
+        dtype: torch.dtype
 ):
     path = Path(path)
     assert len(part) >= 1, "Part must have at least one element, e.g. 'full', 'start', 'mid', or 'end'"
     assert part[0] in ['full', 'start', 'mid', 'end'], "Part must start with one of: 'full', 'start', 'mid' or 'end'"
 
     if part[0] == 'mid':
-        return load_mid_as_state_dict(path, part)
-
+        return load_mid_as_state_dict('mid', path, part, dtype)
 
