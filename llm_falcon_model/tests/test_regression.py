8f7cfe10dff6760847640488ae833bea5c1fe0c4
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-08 23:43:44 +0200

    Added test for regression for Falcon7b.

--- llm_falcon_model/tests/test_regression.py
+++ llm_falcon_model/tests/test_regression.py
@@ -1,6 +1,21 @@
 import joblib
+import torch
+
+from llm_falcon_model import load_config, FalconFull
+from llm_sepweight import load_as_state_dict
+import state_dict_paths
 
 
 def test_regression():
-    reference = joblib.load('ref-falcon-7b.pkl')
-    print(len(reference))
+    config = load_config('7b')
+    state_dict = load_as_state_dict(
+        path=state_dict_paths.falcon_7b,
+        part=('full', str(config.num_hidden_layers)),
+        dtype=torch.bfloat16
+    )
+    torch.set_default_device(torch.device("cuda:0"))
+    torch.set_default_dtype(torch.bfloat16)
+    model = FalconFull(config).eval()
+    model.load_state_dict(state_dict)
+    reference = joblib.load(state_dict_paths.falcon_7b_ref)
+    assert (model(reference['input_ids']) - reference['logits']).abs().mean().item() < 0.02
--- llm_sepweight/src/llm_sepweight/__init__.py
+++ llm_sepweight/src/llm_sepweight/__init__.py
@@ -2,3 +2,4 @@ __version__ = "0.5.0"
 
 from .state_dict_separation import dump_to_directory
 from .deciders import falcon_decider
+from .loading import load_as_state_dict
