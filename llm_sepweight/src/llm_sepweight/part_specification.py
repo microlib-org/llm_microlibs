690c61f58389951bd5a2c13fada827e85d634c16
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-16 00:50:25 +0200

    Parsing of part specification.

--- llm_sepweight/src/llm_sepweight/part_specification.py
+++ llm_sepweight/src/llm_sepweight/part_specification.py
@@ -11,10 +11,19 @@ class PartSpec:
     @classmethod
     def from_string(cls, raw: str):
         parts = raw.split()
+        assert len(parts) > 0, f"You need to provide at least one part to load: '{raw}'"
+        if parts[0] == 'f':
+            # If user wants to load full model
+            num_layers = int(parts[1])
+            return cls(begin=True, mid=[range(num_layers)], end=True)
         begin = 'b' == parts[0]
         end = 'e' == parts[-1]
         ranges = []
         for part in parts:
+            if '-' not in part:
+                continue
             start, stop = map(int, part.split('-'))
             ranges.append(range(start, stop))
+        if begin and len(ranges) > 0:
+            assert ranges[0].start == 0, 'When loading begin, first range must start with 0'
         return cls(begin=begin, mid=ranges, end=end)
