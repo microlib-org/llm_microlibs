40d7826595613ba8b0418d3d24627ed68b1627f9
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-17 14:14:03 +0200

    PartSpec is_full attribute.

--- llm_sepweight/src/llm_sepweight/part_specification.py
+++ llm_sepweight/src/llm_sepweight/part_specification.py
@@ -7,6 +7,7 @@ class PartSpec:
     begin: bool
     mid: List[range]
     end: bool
+    is_full: bool
 
     @classmethod
     def from_string(cls, raw: str):
@@ -15,7 +16,7 @@ class PartSpec:
         if parts[0] == 'f':
             # If user wants to load full model
             num_layers = int(parts[1])
-            return cls(begin=True, mid=[range(num_layers)], end=True)
+            return cls(begin=True, mid=[range(num_layers)], end=True, is_full=True)
         begin = 'b' == parts[0]
         end = 'e' == parts[-1]
         ranges = []
@@ -26,4 +27,4 @@ class PartSpec:
             ranges.append(range(start, stop))
         if begin and len(ranges) > 0:
             assert ranges[0].start == 0, 'When loading begin, first range must start with 0'
-        return cls(begin=begin, mid=ranges, end=end)
+        return cls(begin=begin, mid=ranges, end=end, is_full=False)
