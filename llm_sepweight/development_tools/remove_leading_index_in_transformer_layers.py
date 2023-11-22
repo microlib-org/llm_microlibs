b80a967643a2bf1044647122ca08b31827089380
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-22 19:54:58 +0200

    Added script for removal of leading indices in transformer layers.

---
+++ llm_sepweight/development_tools/remove_leading_index_in_transformer_layers.py
@@ -0,0 +1,32 @@
+import argparse
+import logging
+from pathlib import Path
+
+import torch
+
+from llm_sepweight import migrate
+from tqdm import tqdm
+
+
+def remove_leading_index(path):
+    filenames = sorted(Path(path).glob('mid.*'))
+    for filename in tqdm(filenames):
+        layer_state_dict = torch.load(filename)
+        migrated = {}
+        for k, v in layer_state_dict.items():
+            new_k = ('.'.join(k.split('.')[2:]))
+            migrated[new_k] = v
+        torch.save(migrated, filename)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Script for removing the leading index in transformer layers")
+    parser.add_argument('path', type=str, help='Path to the directory containing the state dictionaries')
+    args = parser.parse_args()
+    path = Path(args.path)
+    migrate(path)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    main()
