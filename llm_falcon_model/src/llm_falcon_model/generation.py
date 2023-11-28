adf841eb085c38f0e349413ce5a532c847dd18eb
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-28 15:01:05 +0200

    Moving to device is optional.

--- llm_falcon_model/src/llm_falcon_model/generation.py
+++ llm_falcon_model/src/llm_falcon_model/generation.py
@@ -9,18 +9,19 @@ import llm_sampler
 from tokenizers import Tokenizer
 
 
-def generate_local(
+def generate(
         input_text: str,
         tokenizer: Tokenizer,
-        device: str,
         forward_func: nn.Module,
         max_new_tokens: int,
         temperature: float,
         warp_top_k: Optional[int] = 10,
+        device: Optional[str] = None,
 ) -> str:
     input_ids = tokenizer.encode(input_text).ids
     batch = torch.tensor(input_ids).unsqueeze(0)
-    batch = batch.to(device)
+    if device:
+        batch = batch.to(device)
     result_tokens = deepcopy(input_ids)
     gen = llm_sampler.sample(
         forward_func=forward_func,
@@ -30,5 +31,5 @@ def generate_local(
         warp_top_k=warp_top_k
     )
     for tok in tqdm(gen, total=max_new_tokens):
-        result_tokens.append(tok.item())
+        result_tokens.append(tok.detach().cpu().item())
     return tokenizer.decode(result_tokens)
