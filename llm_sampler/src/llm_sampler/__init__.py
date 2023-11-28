8e3cdffc02e5cbc3fa502fbacb39d929646d28aa
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-28 10:32:45 +0200

    Build continuation naive.

--- llm_sampler/src/llm_sampler/__init__.py
+++ llm_sampler/src/llm_sampler/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "0.2.1"
 
 from .base import sample, score_batch, score_batch_iterative
+from .utils import best_continuation_naive
--- llm_sampler/src/llm_sampler/utils.py
+++ llm_sampler/src/llm_sampler/utils.py
@@ -1,16 +1,32 @@
-from typing import List
+import torch
 
 
-def build_intermediary_continuations(
-        input_ids: List[int],
-        all_continuation_ids: List[List[int]]
-) -> List[List[int]]:
-    results = [input_ids.copy()]
-    for continuation in all_continuation_ids:
-        # start with just the input_ids
-        current_combination = input_ids.copy()
-        # extend the current_combination by adding elements of the continuation one-by-one
-        for item in continuation:
-            current_combination.append(item)
-            results.append(current_combination.copy())
-    return results
+def best_continuation_naive(scores: torch.Tensor, epsilon: float):
+    """
+    Find the best continuation based on the provided logits, considering ties, using PyTorch.
+
+    :param scores: A 2D PyTorch tensor where each row is a continuation and each column is a logit score for a token.
+    :param epsilon: The tolerance for considering logits as equally good.
+    :return: The index of the best continuation.
+    """
+    num_continuations, num_tokens = scores.shape
+
+    # Start with all continuations as candidates
+    candidates = torch.arange(num_continuations)
+
+    for token_index in range(num_tokens):
+        # Extract logits for the current token among the remaining candidates
+        current_logits = scores[candidates, token_index]
+
+        # Find the maximum logit for the current token
+        max_logit = torch.max(current_logits)
+
+        # Update candidates to those within epsilon of the maximum
+        candidates = candidates[torch.abs(current_logits - max_logit) <= epsilon]
+
+        # If only one candidate remains, return it
+        if len(candidates) == 1:
+            return candidates[0]
+
+    # If multiple candidates remain at the end, handle as required
+    return candidates[0] if len(candidates) > 0 else None
