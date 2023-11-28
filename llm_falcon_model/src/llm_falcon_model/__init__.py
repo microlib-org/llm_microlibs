b4397ad75da69ae46fc969b2b27de5d2e25fc8cb
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-28 15:50:56 +0200

    Bump version, expose generate function.

--- llm_falcon_model/src/llm_falcon_model/__init__.py
+++ llm_falcon_model/src/llm_falcon_model/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 
 from .modelling_RW import FalconBegin, FalconEnd, DecoderSingleLayerNorm, DecoderTwoLayerNorm
 from .configuration_RW import load_config
 from .tokenization import load_tokenizer
 from .deciders import falcon_decider
 from .initialization import init_part
+from .generation import generate
 
