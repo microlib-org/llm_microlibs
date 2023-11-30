f070e078ea35a2d0ebd306541ab132ebc0b30fe1
Hristo Vrigazov <hvrigazov@gmail.com>
2023-11-30 10:31:47 +0200

    Moved HF stuff to a separate file.

--- llm_falcon_model/development_tools/compare_7b_to_original.py
+++ llm_falcon_model/development_tools/compare_7b_to_original.py
@@ -1,43 +1,24 @@
 import torch
-import transformers
-from transformers import AutoTokenizer
 
 import llm_falcon_model
 import llm_sepweight
 
 
-def load_huggingface_pipeline():
-    model = "tiiuae/falcon-7b"
-
-    tokenizer = AutoTokenizer.from_pretrained(model)
-
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        torch_dtype=torch.bfloat16,
-        trust_remote_code=True,
-        # device_map="auto",
-        device=torch.device("cuda")
-    )
-    return pipeline
-
-
 def load_model():
-    part = llm_falcon_model.init_part('7b', 'f', 'cuda:1')
+    part = llm_falcon_model.init_part('7b', 'f', 'cuda:0')
     state_dict = llm_sepweight.load('/home/hvrigazov/llm-sepweights/falcon-7b', 'f').to_dict()
     part.load_state_dict(state_dict)
     return part
 
 
 def main():
-    pipeline = load_huggingface_pipeline()
+    reference_7b = torch.load('reference_7b.pth')
+    tokenizer = llm_falcon_model.load_tokenizer()
     model = load_model()
-    input_ids = torch.tensor(pipeline.tokenizer.encode("Magnus Carlsen won the World ")).unsqueeze(0).cuda()
+    input_ids = torch.tensor(tokenizer.encode("Magnus Carlsen won the World ").ids).unsqueeze(0).cuda()
     with torch.inference_mode():
-        logits_hf = pipeline.model(input_ids)
-        logits_new = model(input_ids.cuda(1))
-        assert torch.allclose(logits_new.cuda(0), logits_hf.logits)
+        logits_new = model(input_ids)
+        assert torch.allclose(logits_new, reference_7b['logits'].cuda())
         print('OK')
 
 
