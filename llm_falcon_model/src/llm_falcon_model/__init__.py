5ae0f1e772f21fa7c6fddf95feec6b01504e0365
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-02 17:05:25 +0200

    Re-export config.

--- llm_falcon_model/src/llm_falcon_model/__init__.py
+++ llm_falcon_model/src/llm_falcon_model/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.7.0"
 
-from .modelling_RW import FalconBegin, FalconEnd, DecoderSingleLayerNorm, DecoderTwoLayerNorm
+from .modelling_RW import FalconBegin, FalconEnd, DecoderSingleLayerNorm
 from .configuration_RW import load_config
 from .tokenization import load_tokenizer
 from .deciders import falcon_decider
