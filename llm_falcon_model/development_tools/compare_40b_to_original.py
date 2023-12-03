5029a7655a651a73caf80cb62dffc8321ca0fb9b
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-03 12:30:53 +0200

    Update compare 40b to save memory.

--- llm_falcon_model/development_tools/compare_40b_to_original.py
+++ llm_falcon_model/development_tools/compare_40b_to_original.py
@@ -1,6 +1,7 @@
 import logging
 
 import torch
+from tqdm import tqdm
 
 import llm_falcon_model
 import llm_sepweight
@@ -9,7 +10,12 @@ from llm_falcon_model.modeling_updated import prepare_for_forward_full_sequence
 
 def load_model():
     part = llm_falcon_model.init_part('40b', 'f', 'cpu')
-    part.load_state_dict(llm_sepweight.load('/home/hvrigazov/llm-sepweights/falcon-40b', 'f').to_dict())
+    part_specs = ['b', 'e']
+    for i in range(len(part.mid)):
+        part_specs.append(f'{i}-{i+1}')
+    for part_spec in tqdm(part_specs):
+        state_dict = llm_sepweight.load('/home/hvrigazov/llm-sepweights/falcon-40b', part_spec).to_dict()
+        part.load_state_dict(state_dict, strict=False)
     return part
 
 
@@ -22,7 +28,7 @@ def main():
     prepare_for_forward_full_sequence(input_ids, model.mid.values())
     with torch.inference_mode():
         logits_new = model(input_ids)
-        assert torch.allclose(logits_new, reference['logits'].cuda())
+        assert torch.allclose(logits_new, reference['logits'].cpu())
         print('OK')
 
 
