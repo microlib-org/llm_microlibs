b1a655afe13246a76b1fb6700da9eecf200a7204
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-05 14:51:09 +0200

    Move to device, final node implementation.

--- llm_falcon_model/src/llm_falcon_model/distributed.py
+++ llm_falcon_model/src/llm_falcon_model/distributed.py
@@ -8,20 +8,27 @@ from llm_sepweight import Part
 
 
 class FalconNode:
-    def __init__(self, part: Part, device: str, client: Optional[RPCClient] = None):
+    def __init__(
+            self,
+            part: Part,
+            device: str,
+            next_node: Optional[RPCClient] = None,
+            final_node: Optional[RPCClient] = None
+    ):
         self.part = part
         self.device = device
-        self.client = client
+        self.next_node = next_node
+        self.final_node = final_node
 
     def clear_cache(self):
-        if self.client is not None:
-            self.client.clear_cache()
+        if self.next_node is not None:
+            self.next_node.clear_cache()
         for block in self.part.mid.values():
             block.self_attention.layer_past = None
 
     def prepare_for_full_sequence(self, input_shape: torch.Size):
-        if self.client is not None:
-            self.client.prepare_for_full_sequence(input_shape)
+        if self.next_node is not None:
+            self.next_node.prepare_for_full_sequence(input_shape)
         mid = list(self.part.mid.values())
         position_ids = torch.arange(0, input_shape[1], dtype=torch.long).unsqueeze(0)
         attention_mask = _prepare_4d_causal_attention_mask(
@@ -35,8 +42,8 @@ class FalconNode:
             block.self_attention.position_ids = position_ids
 
     def prepare_for_single_forward(self, input_shape: torch.Size, position_ids: torch.Tensor):
-        if self.client is not None:
-            self.client.prepare_for_single_forward(input_shape, position_ids)
+        if self.next_node is not None:
+            self.next_node.prepare_for_single_forward(input_shape, position_ids)
         mid = list(self.part.mid.values())
         attention_mask = _prepare_4d_causal_attention_mask(
             attention_mask=None,
@@ -51,9 +58,12 @@ class FalconNode:
 
     @torch.inference_mode()
     def forward(self, x:  torch.Tensor):
+        x = x.to(self.device)
         x = self.part(x)
-        if self.client is not None:
-            self.client.forward(x)
+        if self.next_node is not None:
+            self.next_node.forward(x.cpu())
+        if self.final_node is not None:
+            self.final_node.receive_result(x.cpu())
         return x
 
     def forward_full_sequence(self, input_ids):
