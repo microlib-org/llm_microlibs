439a52b2de5288df46042ba52642e8e74c9b956d
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-07 14:35:41 +0200

    Run part with different kwargs.

--- llm_falcon_model/src/llm_falcon_model/run_part.py
+++ llm_falcon_model/src/llm_falcon_model/run_part.py
@@ -1,31 +1,14 @@
 import logging
 
-import torch
 import socket_rpc
 
 import llm_falcon_model
 import llm_sepweight
-
 from llm_falcon_model.distributed import FalconNode
 
 
-def main():
-    model_name = '40b'
-    spec = '0-17'
-    device = 'cuda:0'
-    path = f'/home/hvrigazov/llm-sepweights/falcon-{model_name}/'
-    next_node_port = 61001
-    final_node_port = 61002
-    logging.basicConfig(level=logging.DEBUG)
-
-    part = llm_falcon_model.init_part(model_name, spec, device)
-    part_state_dict = llm_sepweight.load(path, spec)
-    part.load_state_dict(part_state_dict.to_dict())
-
-    client = socket_rpc.RPCClient('localhost', 61001)
-    node = FalconNode(part, device, client)
-
-    server = socket_rpc.RPCServer('localhost', 61001, 1 * 1024 * 1024)
+def serve_node(node):
+    server = socket_rpc.RPCServer('localhost', 61001)
     server.add_fn(node.clear_cache)
     server.add_fn(node.prepare_for_full_sequence)
     server.add_fn(node.prepare_for_single_forward)
@@ -33,6 +16,31 @@ def main():
     server.add_fn(node.forward_full_sequence)
     server.add_fn(node.forward_single)
     server.serve()
+
+
+def main():
+    model_name = '7b'
+    spec = '16-32 e'
+    device = 'cuda:0'
+    path = f'/home/hvrigazov/llm-sepweights/falcon-{model_name}/'
+    next_node_port = None
+    final_node_port = 61002
+    logging.basicConfig(level=logging.DEBUG)
+
+    part = llm_falcon_model.init_part(model_name, spec, device)
+    part_state_dict = llm_sepweight.load(path, spec)
+    part.load_state_dict(part_state_dict.to_dict())
+
+    kwargs = {
+        'part': part,
+        'device': device
+    }
+    if next_node_port is not None:
+        kwargs['next_node'] = socket_rpc.RPCClient('localhost', next_node_port)
+    elif final_node_port is not None:
+        kwargs['final_node'] = socket_rpc.RPCClient('localhost', final_node_port)
+    node = FalconNode(**kwargs)
+    serve_node(node)
 
 
 if __name__ == '__main__':
