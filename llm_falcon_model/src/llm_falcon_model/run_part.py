29c1daae99eb41298e56efe8d8ab08e439b2c6d9
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-07 14:45:37 +0200

    Use ArgumentParser.

--- llm_falcon_model/src/llm_falcon_model/run_part.py
+++ llm_falcon_model/src/llm_falcon_model/run_part.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 import socket_rpc
@@ -7,8 +8,8 @@ import llm_sepweight
 from llm_falcon_model.distributed import FalconNode
 
 
-def serve_node(node):
-    server = socket_rpc.RPCServer('localhost', 61001)
+def serve_node(node, server_port):
+    server = socket_rpc.RPCServer('localhost', server_port)
     server.add_fn(node.clear_cache)
     server.add_fn(node.prepare_for_full_sequence)
     server.add_fn(node.prepare_for_single_forward)
@@ -19,28 +20,33 @@ def serve_node(node):
 
 
 def main():
-    model_name = '7b'
-    spec = '16-32 e'
-    device = 'cuda:0'
-    path = f'/home/hvrigazov/llm-sepweights/falcon-{model_name}/'
-    next_node_port = None
-    final_node_port = 61002
+    parser = argparse.ArgumentParser(description="Set up and run a FalconNode server.")
+    parser.add_argument('--model_name', required=True, help='Name of the model, e.g., 7b')
+    parser.add_argument('--spec', required=True, help='Specification, e.g., 16-32 e')
+    parser.add_argument('--device', required=True, help='Device to use, e.g., cuda:0')
+    parser.add_argument('--path', required=True, help='Path to the model weights')
+    parser.add_argument('--server_port', type=int, required=True, help='Port for the server to listen on')
+    parser.add_argument('--next_node_port', type=int, help='Port of the next node in the chain')
+    parser.add_argument('--final_node_port', type=int, help='Port of the final node in the chain')
+
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.DEBUG)
 
-    part = llm_falcon_model.init_part(model_name, spec, device)
-    part_state_dict = llm_sepweight.load(path, spec)
+    part = llm_falcon_model.init_part(args.model_name, args.spec, args.device)
+    part_state_dict = llm_sepweight.load(args.path, args.spec)
     part.load_state_dict(part_state_dict.to_dict())
 
     kwargs = {
         'part': part,
-        'device': device
+        'device': args.device
     }
-    if next_node_port is not None:
-        kwargs['next_node'] = socket_rpc.RPCClient('localhost', next_node_port)
-    elif final_node_port is not None:
-        kwargs['final_node'] = socket_rpc.RPCClient('localhost', final_node_port)
+    if args.next_node_port is not None:
+        kwargs['next_node'] = socket_rpc.RPCClient('localhost', args.next_node_port)
+    elif args.final_node_port is not None:
+        kwargs['final_node'] = socket_rpc.RPCClient('localhost', args.final_node_port)
     node = FalconNode(**kwargs)
-    serve_node(node)
+    serve_node(node, args.server_port)
 
 
 if __name__ == '__main__':
