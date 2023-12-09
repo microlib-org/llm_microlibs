dbc65737635ea7e68c28f3d19ff271f094e3b455
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-09 13:59:47 +0200

    Accept params.

--- deployment/upload_part_weights.sh
+++ deployment/upload_part_weights.sh
@@ -1,13 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Define variables
-SOURCE_DIR="homebastion.local:/home/hvrigazov/llm-sepweights/falcon-7b"
-DEST_DIR="/home/hvrigazov/llm-sepweights/falcon-7b"
-SPEC="16-32 e"
+# Check if three arguments are provided
+if [ $# -ne 3 ]; then
+    echo "Error: Usage: $0 <source_directory> <destination_directory> <spec>"
+    exit 1
+fi
+
+# Assign arguments to variables
+SOURCE_DIR="$1"
+DEST_DIR="$2"
+SPEC="$3"
+
+# Define the Python module
 PYTHON_MODULE="llm_sepweight.filenames"
 
 # Execute the Python command and loop over its output
-for i in $(python -m $PYTHON_MODULE "$PYTHON_ARGS"); do
+for i in $(python -m $PYTHON_MODULE "$SPEC"); do
     # Perform the rsync operation
     rsync -avz --progress "$SOURCE_DIR/$i" "$DEST_DIR/$i"
 
