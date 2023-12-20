cdbc0a3b1bcfcf447c904313c7855b827b9e6f61
Hristo Vrigazov <hvrigazov@gmail.com>
2023-12-20 20:17:00 +0200

    Expose get_filenames.

--- llm_sepweight/src/llm_sepweight/__init__.py
+++ llm_sepweight/src/llm_sepweight/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.9.4"
+__version__ = "0.10.0"
 
 from .dumping import dump
 from .loading import load, lazy_load
 from .utils import is_cpu_memory_less_than_gpu
 from .part_module import Part
 from .part_state_dict import PartStateDict
+from .filenames import get_filenames
